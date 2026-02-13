55bc12692bb9f4d6abb67514927762e08d7b4e5f
David Wells <rwells42@rrc.ca>
2026-02-12 20:03:32 -0600

    Added all cases to be filled out.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let currentState = start
+let currentState = Start
 
 function renderQuestion() {
     let question = document.getElementById("question")
     let answer = document.getElementById("answers")
     let nextButton = document.getElementById("next-button")
+    switch (currentState) {
+        case "Start":
+        case "Heads":
+        case "Higher than 8":
+        case "Win $20":
+        case "Lower than 8":
+        case "Lost even":
+        case "Tails":
+        case "DoubleY":
+        case "Heads":
+        case "Lost again":
+        case "Tails":
+        case "Won Even":
+        case "DoubleN":
+        
+    }
 }
 
 function addAnswerButton(label, nextState) {
